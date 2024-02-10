9251fc24fe79c24c4f468a0b0da9f1c4419a9d0b
al448s <lakin0307@missouristate.edu>
2024-02-09 18:07:22 -0600

    good state

--- app/src/main/java/com/lakin/msu/trafficlight/MainActivity.kt
+++ app/src/main/java/com/lakin/msu/trafficlight/MainActivity.kt
@@ -1,27 +1,53 @@
 package com.lakin.msu.trafficlight
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.lakin.msu.trafficlight.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val lightState = listOf("Go", "Stop", "Wait")
+    private val lightState = listOf("Stop", "Go", "Wait")
+    private val colorChoice = listOf("@color/red", "@color/green", "@color/yellow")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var counter = 1
+        var counter = 0
         binding.changeLightButton.setOnClickListener{
-            listIndex(counter)
-            counter++
+            if (counter < 2) {
+                counter++
+            } else {
+                counter = 0
+
+            }
+            stateName(lightState[counter])
+
         }
     }
 
-    private fun listIndex(index: Int) {
-        val currentIndex = lightState[index]
-        binding.changeLightButton.text = currentIndex
+    private fun stateName(state: String) {
+        binding.changeLightButton.text = state
+        for (i in 0..<lightState.count()) {
+            if (lightState[i] == state) {
+                binding.changeLightButton.setBackgroundColor(Color.parseColor("#FF0000"))
+            }
+        }
+
     }
+
+    /*private fun waitFunc() {
+
+    }
+
+    private fun goFunc() {
+
+    }
+
+    private fun stopFunc() {
+        binding.changeLightButton.setBackgroundColor(Color.RED)
+    }*/
 }
